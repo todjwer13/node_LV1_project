5f7745dc36301101adb4066ff9f2f41a320d5103
todjwer13 <todjwr12@gmail.com>
2023-06-16 04:14:22 +0900

    first commit get, post, put, delete 생성

---
+++ schemas/comments.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  postId: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+})
+
+// postSchema.set("timestamps",true);미국 시간
+
+module.exports = mongoose.model("comments",commentSchema);
---
+++ schemas/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+})
+
+// postSchema.set("timestamps",true); // 미국 시간
+
+module.exports = mongoose.model("posts",postSchema);
